6af98913ee0eca13705ff94b7bde54da5def3fac
Bruno Pierri <pierri95@gmail.com>
2025-05-28 21:28:29 -0300

    removee

--- src/Utils/ShortenerHash.php
+++ src/Utils/ShortenerHash.php
@@ -12,6 +12,7 @@ class ShortenerHash implements ShortenerHashInterface
         $baseConvertTimestamp = substr(base_convert($microtime, 10, 36), 0, 4);
         $randomHex = bin2hex(random_bytes(4));
         $hash = substr(hash('sha512', $url . $randomHex . $baseConvertTimestamp), 0, 8);
+
         $combinedHash = $baseConvertTimestamp . $randomHex . $hash;
 
         return self::toBase62($combinedHash, $length);
